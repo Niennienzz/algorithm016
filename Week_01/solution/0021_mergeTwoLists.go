7ccc8a75941948aa4fcb97508d252ae1a6d198bc
Zhehui (Joe) Zhou <zhehui.zhou@outlook.com>
2020-09-10 23:43:10 -0400

    Week 01 x3

---
+++ Week_01/solution/0021_mergeTwoLists.go
@@ -0,0 +1,36 @@
+package solution
+
+// Description:
+// Merge two sorted linked lists and return it as a new sorted list.
+// The new list should be made by splicing together the nodes of the first two lists.
+//
+// Examples:
+//
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var (
+		before = &ListNode{0, nil}
+		cursor = before
+	)
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			cursor.Next = l1
+			l1 = l1.Next
+		} else {
+			cursor.Next = l2
+			l2 = l2.Next
+		}
+		cursor = cursor.Next
+	}
+
+	if l1 == nil {
+		cursor.Next = l2
+	} else {
+		cursor.Next = l1
+	}
+
+	return before.Next
+}
---
+++ Week_01/solution/0024_swapPairs.go
@@ -0,0 +1,21 @@
+package solution
+
+// Description:
+// Given a linked list, swap every two adjacent nodes and return its head.
+// You may not modify the values in the list's nodes, only nodes itself may be changed.
+//
+// Examples:
+//
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	first, second := head, head.Next
+	first.Next = swapPairs(second.Next)
+	second.Next = first
+
+	return second
+}
