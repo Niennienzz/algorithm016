9c44ce1f63fac967949ceca3547d110918af9c31
Zhehui (Joe) Zhou <zhehui.zhou@outlook.com>
2020-09-17 21:06:55 -0400

    Week 01 0024_swapPairs add iterative solution

--- Week_01/solution/0024_swapPairs.go
+++ Week_01/solution/0024_swapPairs.go
@@ -7,14 +7,38 @@ package solution
 //
 // Given 1->2->3->4, you should return the list as 2->1->4->3.
 
-func swapPairs(head *ListNode) *ListNode {
+func swapPairsRecursive(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
 
 	first, second := head, head.Next
-	first.Next = swapPairs(second.Next)
+	first.Next = swapPairsRecursive(second.Next)
 	second.Next = first
 
 	return second
+}
+
+func swapPairsIterative(head *ListNode) *ListNode {
+	var (
+		preHead = new(ListNode)
+		prev    = preHead
+	)
+	preHead.Next = head
+
+	for head != nil && head.Next != nil {
+		// Nodes to be swapped.
+		first, second := head, head.Next
+
+		// Swap.
+		prev.Next = second
+		first.Next = second.Next
+		second.Next = first
+
+		// Prepare for the next iteration.
+		prev = first
+		head = first.Next
+	}
+
+	return preHead.Next
 }
